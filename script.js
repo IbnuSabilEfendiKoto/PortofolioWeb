25959782ca62b9777d402b356ce4d8b38d31e171
IbnuSabilEfendiKoto <ibnusabile3007@gmail.com>
2023-11-27 23:07:43 -0800

    Pesan komit

---
+++ script.js
@@ -0,0 +1,10 @@
+// SCRIPT NAV RESPONSIVE
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", function () {
+      navLinks.classList.toggle("show");
+    });
+});
+// 
